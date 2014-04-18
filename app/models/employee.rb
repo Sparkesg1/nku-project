532eb214f7189eaab4b415bef4f399b5a04ed8ef
Glenn Sparkes <sparkesg1@nku.edu>
2014-04-18 13:58:00 +0000

    Add validation uniqueness email need redirect

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,6 @@
 class Employee < ActiveRecord::Base
   has_secure_password
-  validates_presence_of :password, :on => :create
+  validates_presence_of :password, :on => :create 
+  validates_uniqueness_of :email, :on => :create
   has_many :request_offs
 end
